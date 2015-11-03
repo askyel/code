cca11db23976df8230065d2a23c8a32c7ccc925e
Ariel Levy <ariel.s.levy1@gmail.com>
2015-11-02 22:43:51 -0500

    updating from Air

---
+++ templates/main.c
@@ -0,0 +1,19 @@
+// <- C Libraries ->
+#include <stdio.h>    // input and output
+#include <fcntl.h>    // read and write files
+#include <errno.h>    // file errors
+#include <unistd.h>   // seek within file
+#include <string.h>   // string functions
+#include <stdlib.h>   // allocate and free memory
+#include <dirent.h>   // read directories
+
+// <- Custom Header Files ->
+//#include "CUSTOM.h"
+
+// <- Custom Functions ->
+
+// <- Main Function ->
+int main() {
+	
+	return 0;
+}
