255a1d4021b468552cd4a801ca05a3b13a3c850c
Ariel Levy <ariel.s.levy1@gmail.com>
2015-11-06 15:28:00 -0500

    updating from Air

---
+++ shell_scripts/repush.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+CUR_DIR=$(pwd)
+
+echo "Pushing to git repositories."
+
+for i in ~/dotfiles\
+	 ~/code\
+	 ~/stuyhs\
+	 ~/code/askyel.github.io\
+	 ; do
+	echo "";
+	echo "$i";
+	cd "$i";
+	git add *;
+	git commit -m "updating from Air"
+	git push;
+	cd $CUR_DIR
+done
+
+echo "";
+echo "...done"
